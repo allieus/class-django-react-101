44ab1223ab8e6446497b0ed953a3144a1e50c972
Chinseok Lee <me@askcompany.kr>
2020-11-02 17:18:31 +0900

    blog 모델/마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimestampModel):
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+
+class Comment(TimestampModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

68a9326021a928a18ba6845099b7a1c9db404b8e
Chinseok Lee <me@askcompany.kr>
2020-11-02 21:36:25 +0900

    django-debug-toolbar 설치

--- myproj/settings.py
+++ myproj/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bootstrap4",
+    "debug_toolbar",
     "blog",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -114,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+INTERNAL_IPS = ["127.0.0.1"]
--- myproj/urls.py
+++ myproj/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings  # django/global_settings.py + myproj.settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -7,3 +8,9 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("", RedirectView.as_view(url="/blog/")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
